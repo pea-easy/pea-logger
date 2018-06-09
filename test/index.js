475cd4b0163668214c23b26facbf3c1022ce3143
vmeng <jmeng2007@163.com>
2018-06-09 15:15:21 +0800

    add define logpath

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 
 const log4jHelper = require('../lib/logger');
-log4jHelper.connect({ name: 'test', level: 'debug' });
+log4jHelper.connect({ name: 'test', level: 'debug', outputPath: './logs' });
 logger.debug(JSON.stringify(process.env))
 logger.info('test success');
 logger.error('it is wrong');
